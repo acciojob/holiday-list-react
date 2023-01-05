44bb49b9b3036fa3e639466eaecb6b9023b47d6a
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-01-05 21:16:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,9 @@
 
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import React, { useState, useEffect } from "react";
+import './../styles/App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.cityList = [{ name: 'Goa', country: 'India' },
+const App = () => {
+    const cityList = [{ name: 'Goa', country: 'India' },
     { name: 'Amsterdam', country: 'Netherlands' },
     { name: 'New York', country: 'USA' },
     { name: 'Darjeeling', country: 'India' },
@@ -35,16 +32,12 @@ class App extends Component {
     { name: 'Mount Abu', country: 'India' },
     { name: 'Tirupati', country: 'India' },
     ]
-  }
-
-  render() {
-    return (
-      <div id="main">
-        {/* Do not remove the main div */}
-      </div>
-    )
-  }
+    
+  return (
+    <div id="main">
+               {/* Do not remove the main div */}
+    </div>
+  )
 }
 
-
-export default App;
+export default App
